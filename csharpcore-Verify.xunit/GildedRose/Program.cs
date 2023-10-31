c65745196980ee4492d7d1f7a59d05a7166beb55
Thomas Twigden <ttwigden@deloitte.co.uk>
2023-10-31 14:55:22 +0000

    Making consistent with main

--- csharpcore-Verify.xunit/GildedRose/Program.cs
+++ csharpcore-Verify.xunit/GildedRose/Program.cs
@@ -1,10 +1,6 @@
 ﻿using GildedRoseKata;
-using System;
-using System.Collections.Generic;
 
-Console.WriteLine("OMGHAI!");
-
-IList<Item> Items = new List<Item>{
+var items = new List<Item>{
     new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
     new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
     new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -32,16 +28,16 @@ IList<Item> Items = new List<Item>{
 	new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
 };
 
-var app = new GildedRose(Items);
+var app = new GildedRose(items);
 
 
 for (var i = 0; i < 31; i++)
 {
     Console.WriteLine("-------- day " + i + " --------");
     Console.WriteLine("name, sellIn, quality");
-    for (var j = 0; j < Items.Count; j++)
+    for (var j = 0; j < items.Count; j++)
     {
-        System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+        Console.WriteLine(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
     }
     Console.WriteLine("");
     app.UpdateQuality();
--- csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
@@ -10,127 +10,127 @@ namespace GildedRoseTests
         [Fact]
         public void UpdateQuality_WhenStandardItem_DecreasesSellInAndQualityBy1()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "StandardItem", SellIn = 10, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "StandardItem", SellIn = 10, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(9);
-            Items[0].Quality.Should().Be(9);
+            items[0].SellIn.Should().Be(9);
+            items[0].Quality.Should().Be(9);
         }
 
         [Fact]
         public void UpdateQuality_WhenStandardItemAndQualityAt0_QualityRemainsAt0()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "StandardItem", SellIn = 10, Quality = 0 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "StandardItem", SellIn = 10, Quality = 0 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(9);
-            Items[0].Quality.Should().Be(0);
+            items[0].SellIn.Should().Be(9);
+            items[0].Quality.Should().Be(0);
         }
 
         [Fact]
         public void UpdateQuality_WhenStandardItemAndNegative_QualityDegradesTwiceAsFast()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "StandardItem", SellIn = 0, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "StandardItem", SellIn = 0, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(-1);
-            Items[0].Quality.Should().Be(8);
+            items[0].SellIn.Should().Be(-1);
+            items[0].Quality.Should().Be(8);
         }
 
         [Fact]
         public void UpdateQuality_WhenAgedBrie_QualityIncreases()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(9);
-            Items[0].Quality.Should().Be(11);
+            items[0].SellIn.Should().Be(9);
+            items[0].Quality.Should().Be(11);
         }
 
         [Fact]
         public void UpdateQuality_WhenAgedBrie_QualityIncreasesTwiceAsFastWhenPastSellbyDate()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(-1);
-            Items[0].Quality.Should().Be(12);
+            items[0].SellIn.Should().Be(-1);
+            items[0].Quality.Should().Be(12);
         }
 
         [Fact]
         public void UpdateQuality_WhenAnyItem_QualityDoesNotGoAbove50()
         {
-            IList<Item> Items = new List<Item>
+            var items = new List<Item>
             {
                 new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 },
                 new Item { Name = "Another Item", SellIn = 10, Quality = 50 }
             };
-            GildedRose app = new(Items);
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(9);
-            Items[0].Quality.Should().Be(50);
-            Items[1].SellIn.Should().Be(9);
-            Items[1].Quality.Should().Be(49);
+            items[0].SellIn.Should().Be(9);
+            items[0].Quality.Should().Be(50);
+            items[1].SellIn.Should().Be(9);
+            items[1].Quality.Should().Be(49);
         }
 
         [Fact]
         public void UpdateQuality_WhenLegendaryItem_SellInAndQualityDoNotChange()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 20, Quality = 80 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 20, Quality = 80 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(20);
-            Items[0].Quality.Should().Be(80);
+            items[0].SellIn.Should().Be(20);
+            items[0].Quality.Should().Be(80);
         }
 
         [Fact]
         public void UpdateQuality_WhenBackstagePasses_QualityIncreasesWhenSellInOver10()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(19);
-            Items[0].Quality.Should().Be(11);
+            items[0].SellIn.Should().Be(19);
+            items[0].Quality.Should().Be(11);
         }
 
         [Fact]
         public void UpdateQuality_WhenBackstagePasses_QualityIncreasesBy2WhenSellInBetween10And6()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(9);
-            Items[0].Quality.Should().Be(12);
+            items[0].SellIn.Should().Be(9);
+            items[0].Quality.Should().Be(12);
         }
 
         [Fact]
         public void UpdateQuality_WhenBackstagePasses_QualityIncreasesBy3WhenSellInBetween5And1()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(4);
-            Items[0].Quality.Should().Be(13);
+            items[0].SellIn.Should().Be(4);
+            items[0].Quality.Should().Be(13);
         }
 
         [Fact]
         public void UpdateQuality_WhenBackstagePasses_QualityGoesTo0WhenSellInReachesNegative1()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 10 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 10 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(-1);
-            Items[0].Quality.Should().Be(0);
+            items[0].SellIn.Should().Be(-1);
+            items[0].Quality.Should().Be(0);
         }
 
         [Fact]
         public void UpdateQuality_WhenConjuredItems_QualityDegradesTwiceAsFast()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Dorito", SellIn = 8, Quality = 20 } };
-            GildedRose app = new(Items);
+            var items = new List<Item> { new Item { Name = "Conjured Dorito", SellIn = 8, Quality = 20 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Items[0].SellIn.Should().Be(7);
-            Items[0].Quality.Should().Be(18);
+            items[0].SellIn.Should().Be(7);
+            items[0].Quality.Should().Be(18);
         }
     }
 }
