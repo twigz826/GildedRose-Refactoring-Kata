eff58a56e09867cd33f1942d0cdcbf93a5ef7b7f
Thomas Twigden <ttwigden@deloitte.co.uk>
2024-01-12 18:34:37 +0000

    Minor refactor to example test

--- csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/GildedRoseTest.cs
@@ -10,7 +10,7 @@ namespace GildedRoseTests
         [Fact]
         public void UpdateQuality_WhenStandardItem_DecreasesSellInAndQualityBy1()
         {
-            var items = new List<Item> { new Item { Name = "StandardItem", SellIn = 10, Quality = 10 } };
+            var items = new List<Item> { new() { Name = "StandardItem", SellIn = 10, Quality = 10 } };
             var app = new GildedRose(items);
             app.UpdateQuality();
             items[0].SellIn.Should().Be(9);
